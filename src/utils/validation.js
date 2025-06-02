211192b97253c0b8fe08f4724334aeaf39353bad
rafaelpuyet <79940385+rafaelpuyet@users.noreply.github.com>
2025-06-02 14:18:21 -0400

    Update validation.js

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -2,10 +2,10 @@
 const isValidEmail = (email) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
 const isValidUsername = (username) => /^[a-zA-Z0-9-]{3,20}$/.test(username);
 const isValidPhoneNumber = (phone_number) => /^\+569\d{8}$/.test(phone_number);
-const isValidPassword = (password) => /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/.test(password);
-const isValidName = (name) => /^[a-zA-Z\s]{2,50}$/.test(name);
+const isValidPassword = (password) => /^(?=.*\p{L})(?=.*\d)[\p{L}\d\W]{8,}$/u.test(password);
+const isValidName = (name) => /^[\p{L}\s]{2,50}$/u.test(name);
 const isValidAddress = (address) => /^.{5,100}$/.test(address);
-const isValidCityCountry = (value) => /^[a-zA-Z\s]{2,50}$/.test(value);
+const isValidCityCountry = (value) => /^[\p{L}\s]{2,50}$/u.test(value);
 const isValidZipcode = (zipcode) => /^[a-zA-Z0-9-]{5,10}$/.test(zipcode);
 
 module.exports = {
