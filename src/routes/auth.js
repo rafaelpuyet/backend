5b658ae167e898fa1e44cfa822ccde6d14305e4b
rafaelpuyet <79940385+rafaelpuyet@users.noreply.github.com>
2025-06-06 17:29:59 -0400

    update

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -10,7 +10,9 @@ const router = express.Router();
 
 // Configuración de Nodemailer
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
